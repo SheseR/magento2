47ef44ad9e836692822bc8050f5d2ecfdc0b2df5
Mykhailo Basenko <basenko.mykhaylo@gmail.com>
2019-02-18 21:46:05 +0200

    [1.5] solve task

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -16,25 +16,30 @@ class Test
     /** @var int  */
     private $number;
 
+    /** @var ManagerInterfaceFactory */
+    private $managerInterfaceFactory;
+
     /**
      * Test constructor.
-     *
      * @param ManagerInterface $manager
      * @param $name
      * @param int $number
      * @param array $arrayList
+     * @param ManagerInterfaceFactory $managerInterfaceFactory
      */
     public function __construct(
         ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        ManagerInterfaceFactory $managerInterfaceFactory
     )
     {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerInterfaceFactory = $managerInterfaceFactory;
     }
 
     /**
@@ -49,5 +54,8 @@ class Test
         print_r($this->number);
         echo '<br/>';
         print_r($this->arrayList);
+        echo '<br/>';
+        $newManager = $this->managerInterfaceFactory->create();
+        print_r(get_class($newManager));
     }
 }
